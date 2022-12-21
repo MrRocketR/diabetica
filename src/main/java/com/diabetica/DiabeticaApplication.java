fe2d05c149ebd09e7bb5940464df336cd1895a85
MrRocketR <kuptsovns@gmail.com>
2022-12-21 23:58:30 +0200

    Initial commit

---
+++ src/main/java/com/diabetica/DiabeticaApplication.java
@@ -0,0 +1,13 @@
+package com.diabetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeticaApplication.class, args);
+	}
+
+}
